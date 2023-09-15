e919ede4c0dff96393bbf1a3241b03aad2b05d46
TomSim0n <tsimon1019@gmail.com>
2023-09-15 13:58:38 +0200

    ex4-2

---
+++ Program4-2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex4_2
+{
+    class student
+    {
+        public string studentId;
+        public float year;
+        public float major;
+
+
+
+    }
+}
+
+
+namespace ex4_2
+{
+    class employee
+    {
+        public double employeeID;
+        public string department;
+        public string title;
+
+
+
+    }
+}
+
+
+namespace ex4_2
+{
+    class department
+    {
+        public double departmentID;
+        public string departmentName;
+
+
+
+    }
+}
+
+
+namespace ex4_2
+{
+    class course
+    {
+        public string courseName;
+        public double coursID;
+        public float credits;
+        public string major;
+
+
+
+    }
+}
+
+
+namespace ex4_2
+{
+    class Classroom
+    {
+        public float ClassroomNo;
+
+
+
+    }
+}
+
